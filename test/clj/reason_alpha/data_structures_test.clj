c2ca100f4843dcd8c4605b11dbfa49559bbce3d3
JDuPreez <https://medium.com/@recursiveenigma>
2020-08-29 21:01:48 +0200

    data-structures/conj-ancestors-path + unit tests completed

---
+++ test/clj/reason_alpha/data_structures_test.clj
@@ -0,0 +1,32 @@
+(ns reason-alpha.data-structures-test
+  (:require [clojure.test :refer :all]
+            [reason-alpha.data-structures :as data-structs]))
+
+(deftest test-data-structures
+  (testing "`data-structures/conj-ancestors-path` should add ancestors path"
+    (let [data                [{:id 1 :name "One" :parent 2}
+                               {:id 2 :name "Two" :parent 4}
+                               {:id 3 :name "Three" :parent 4}
+                               {:id 4 :name "Four" :parent 5}
+                               {:id 5 :name "Five"}]
+          with-ancestors-path (data-structs/conj-ancestors-path
+                               data
+                               :parent
+                               :name)]
+      (is (every? (fn [{:keys [ancestors-path]}]
+                    (some? ancestors-path)) with-ancestors-path))
+      (is (= (:ancestors-path (nth with-ancestors-path 0 nil))
+             '("Five" "Four" "Two")))
+      (is (= (:ancestors-path (nth with-ancestors-path 1 nil))
+             '("Five" "Four")))
+      (is (= (:ancestors-path (nth with-ancestors-path 2 nil))
+             '("Five" "Four")))
+      (is (= (:ancestors-path (nth with-ancestors-path 3 nil))
+             '("Five")))
+      (is (= (:ancestors-path (nth with-ancestors-path 4 nil))
+             '())))))
+
+(comment
+  (clojure.test/run-tests 'reason-alpha.data-structures-test)
+
+)
