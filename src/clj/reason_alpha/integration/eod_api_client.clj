2531e5bd4419079e0875ef15e8da7242ba32bca1
JduPreez <514562+JduPreez@users.noreply.github.com>
2022-07-01 17:41:07 +0200

    WIP

--- src/clj/reason_alpha/integration/eod_api_client.clj
+++ src/clj/reason_alpha/integration/eod_api_client.clj
@@ -2,7 +2,9 @@
   (:require [ajax.core :refer [GET]]
             [cheshire.core :as json]
             [clojure.string :as str]
-            [outpace.config :refer [defconfig]]))
+            [outpace.config :refer [defconfig]]
+            [reason-alpha.utils :as utils]
+            [tick.core :as tick]))
 
 ;; TODO: Get this to each user's own profile
 (defconfig api-token)
@@ -10,10 +12,31 @@
 (defconfig live-stock-prices-api)
 
 (defn- handle-quote-live-price [*result idx-hid-tkrs response]
-  (clojure.pprint/pprint {::handle-quote-live-price response})
-  (deliver *result
-           {:result response
-            :type   :success}))
+  ;; (clojure.pprint/pprint {::handle-quote-live-price response})
+
+  (let [r (map (fn [{:keys [code timestamp open previousClose
+                            high low volume change close] :as quote}]
+                 (clojure.pprint/pprint quote)
+                 (let [ptime (-> timestamp
+                                 (tick/new-duration :seconds)
+                                 tick/inst)]
+                   {:price-id             (utils/new-uuid)
+                    :price-creation-id    (utils/new-uuid)
+                    :symbol-ticker        code
+                    :symbol-provider      :eod-historical-data
+                    :holding-id           (get idx-hid-tkrs code)
+                    :price-time           ptime
+                    :price-open           open
+                    :price-close          close
+                    :price-high           high
+                    :price-low            low
+                    :price-previous-close previousClose
+                    :price-volume         volume
+                    :price-change         change})) response)]
+    (deliver *result
+             {:result {:response     r
+                       :idx-hid-tkrs idx-hid-tkrs}
+              :type   :success})))
 
 (defn- handle-quote-live-price-err
   [*result idx-hid-tkrs {:keys [status status-text]
@@ -85,5 +108,43 @@
     (doall
      (for [*r results]
        @*r)))
+
+  (let [response     '({:open          166.82,
+                        :close         167.76,
+                        :volume        212094,
+                        :high          169.8,
+                        :gmtoffset     0,
+                        :code          "ADS.XETRA",
+                        :previousClose 168.76,
+                        :low           165.7,
+                        :change_p      -0.5926,
+                        :change        -1,
+                        :timestamp     1656681420}
+                       {:open          360,
+                        :close         354.4,
+                        :volume        33385197,
+                        :high          363.6,
+                        :gmtoffset     0,
+                        :code          "0700.HK",
+                        :previousClose 365,
+                        :low           354,
+                        :change_p      -2.9041,
+                        :change        -10.6,
+                        :timestamp     1656576480}
+                       {:open          76.01,
+                        :close         76.39,
+                        :volume        6504025,
+                        :high          76.88,
+                        :gmtoffset     0,
+                        :code          "SBUX.US",
+                        :previousClose 76.43,
+                        :low           74.87,
+                        :change_p      -0.0523,
+                        :change        -0.04,
+                        :timestamp     1656619200})
+        idx-hid-tkrs {"ADS.XETRA" 1, "0700.HK" 2, "SBUX.US" 3}
+        *result      (promise)]
+    (handle-quote-live-price *result idx-hid-tkrs response)
+    @*result)
 
   )
