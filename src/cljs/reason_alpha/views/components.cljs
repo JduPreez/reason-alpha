5160dadf5b6b4b86db15c0de32049ed9680eca5e
JduPreez <514562+JduPreez@users.noreply.github.com>
2023-06-10 17:55:41 +0400

    WIP

---
+++ src/cljs/reason_alpha/views/components.cljs
@@ -0,0 +1,75 @@
+(ns reason-alpha.views.components
+  (:require [cljs-uuid-utils.core :as uuid]
+            [medley.core :as medley]
+            [re-com.core :as re-com]
+            [re-com.dropdown :as dropdown]
+            [re-com.dropdown :as dropdown]
+            [re-frame.core :as rf]
+            [reagent.core :as reagent]
+            [reason-alpha.utils :as utils]))
+
+(defn val->choice-id [ctype v]
+  (if (= ctype :keyword)
+    (utils/keyword->str v)
+    (str v)))
+
+(defn choice-type [v]
+  (cond
+    (keyword? v)
+    , :keyword
+
+    (uuid/valid-uuid? v)
+    , :uuid
+
+    (medley/boolean? v)
+    , :bool
+
+    :else :string))
+
+(defn choice-id->val [ctype choices cid]
+  (let [label (some (fn [{:keys [id label]}]
+                      (when (= id cid) label)) choices)
+        id    (case ctype
+                :keyword (keyword cid)
+                :uuid    (medley/uuid cid)
+                :bool    (utils/str->bool cid)
+                cid)]
+    [id label]))
+
+(def ref-suffix "ref")
+(def ref-suffix-list (str ref-suffix "-list"))
+
+(defn select-dropdown
+  ([schema]
+   (when schema
+     (cljs.pprint/pprint {:>>> schema})))
+  ([model-type idk *r {data-sub :data-subscription
+                       :as      field}]
+   (let [id            (idk @*r)
+         ;; *r                (rf/subscribe [:form/edited-record-by-pk form id])
+         *choices      (if data-sub
+                         (rf/subscribe data-sub)
+                         #_else (reagent/atom []))
+         ctype         (-> @*choices first :id choice-type)
+         *selected-val (reagent/atom (->> field
+                                          :name
+                                          (get @*r)
+                                          first
+                                          (val->choice-id ctype)))]
+     [:span @*selected-val]
+     #_(fn [form model-type idk *r _]
+         (let [choices (map (fn [{:keys [id] :as c}]
+                              (update
+                               c :id
+                               #(val->choice-id ctype %)))
+                            @*choices)]
+           (cljs.pprint/pprint {::select-dropdown choices})
+           [dropdown/single-dropdown
+            :src (re-com/at)
+            :choices choices
+            :model       *selected-val
+            :width       "100%"
+            :max-height  "200px"
+            :class       "form-control"
+            :style       {:padding "0"}
+            :filter-box? true])))))
